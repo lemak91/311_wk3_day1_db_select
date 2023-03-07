14e3a67620885a6e195e889e63d69a7acc95e922
John Lemak <lemak91@mac.com>
2023-03-07 17:25:52 -0600

    done

---
+++ solution.sql
@@ -0,0 +1,7 @@
+SELECT * FROM admin.users;
+
+SELECT * FROM admin.users WHERE city = 'Chicago';
+
+SELECT state FROM admin.users WHERE first_name = 'Glory';
+
+SELECT * FROM admin.users WHERE phone1 LIKE '773%';
